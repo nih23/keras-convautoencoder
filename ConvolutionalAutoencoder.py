fb1e2bea29d05d248c4a5bd6564f29cc8398b228
nih23 <nico.hoffmann@tu-dresden.de>
2017-10-11 17:31:32 +0200

    convae learning+prediction w/ keras2 works, loading still buggy

--- ConvolutionalAutoencoder.py
+++ ConvolutionalAutoencoder.py
@@ -26,38 +26,42 @@ def build_model(nb_filters=32, nb_pool=2, nb_conv=2):
     mp =MaxPooling2D(pool_size=(nb_pool, nb_pool))
     # =========      ENCODER     ========================
     model.add(c)
-    model.add(Activation('tanh'))
+    model.add(Activation('relu'))
     model.add(mp)
     model.add(Dropout(0.25))
     # =========      BOTTLENECK     ======================
     model.add(Flatten())
     model.add(d)
-    model.add(Activation('tanh'))
+    model.add(Activation('relu'))
     # =========      BOTTLENECK^-1   =====================
-    model.add(DependentDense(nb_filters * 14 * 14, d))
-    model.add(Activation('tanh'))
+    model.add(DependentDense(units=nb_filters * 14 * 14, master_layer=d))
+    model.add(Activation('relu'))
     model.add(Reshape((nb_filters, 14, 14)))
     # =========      DECODER     =========================
     model.add(DePool2D(mp, size=(nb_pool, nb_pool)))
-    model.add(Deconvolution2D(c, border_mode='same'))
-    model.add(Activation('tanh'))
+    model.add(Deconvolution2D(binded_conv_layer=c, border_mode='same', filters = nb_filters, kernel_size = (nb_conv, nb_conv)))
+    model.add(Activation('relu'))
 
     return model
 
 
 if __name__ == '__main__':
+    pModel = 'convae.h5'
+    noEpochs = 10
+    batchSz = 12000
     (X_train, y_train), (X_test, y_test) = load_data()
     X_train = np.swapaxes(X_train,1,3)
-    print("SHAPE " + str(X_train.shape))
+    X_test = np.swapaxes(X_test,1,3)
     model = build_model()
     if not False:
         model.compile(optimizer='rmsprop', loss='mean_squared_error')
         model.summary()
-        model.fit(X_train, X_train, nb_epoch=50, batch_size=512, validation_split=0.2,
+        model.fit(X_train, X_train, epochs=noEpochs, batch_size=batchSz, validation_split=0.2,
                   callbacks=[EarlyStopping(patience=3)])
-        model.save_weights('./conv.neuro', overwrite=True)
+        model.save(pModel, overwrite=True)
     else:
-        model.load_weights('./conv.neuro')
+        #TODO: fix me.. broken
+        model = models.load_model(pModel, custom_objects={"DependentDense": DependentDense, "Deconvolution2D": Deconvolution2D, "DePool2D": DePool2D})
         model.compile(optimizer='rmsprop', loss='mean_squared_error')
 
     show_representations(model, X_test)
--- autoencoder_layers.py
+++ autoencoder_layers.py
@@ -51,31 +51,22 @@ class DePool2D(UpSampling2D):
     def call(self, inputs):
         X = inputs
         if self.data_format == 'channels_first':
-#            print('DePool2D: th')
             output = K.repeat_elements(X, self.size[0], axis=2)
             output = K.repeat_elements(output, self.size[1], axis=3)
         elif self.data_format == 'channels_last':
-#            print('DePool2D: tf')
             output = K.repeat_elements(X, self.size[0], axis=2)
             output = K.repeat_elements(output, self.size[1], axis=3)
             output = K.permute_dimensions(output, (0, 2, 3, 1))
         else:
             raise Exception('Invalid dim_ordering: ' + self.dim_ordering)
 
-        #f = T.grad(T.sum(self._pool2d_layer.get_activations(inputs)), wrt=self._pool2d_layer.call(inputs)) * output
-#        print("input -> " + str(X.shape))
-#        print("output -> " + str(output.shape))
         f = K.gradients(loss=K.sum(self._pool2d_layer.output), variables=self._pool2d_layer.input) * output
         f = K.squeeze(f, axis=0)
         print(str(K.get_variable_shape(f)))
         return f
 
     def compute_output_shape(self, input_shape):
- #       print("input -> " + str(input_shape))
- #       print("ups -> " + str(input_shape[2] * self.size[0]))
-
         output_shape = (input_shape[0], input_shape[2] * self.size[0], input_shape[3] * self.size[1], input_shape[1])
-        print("depool out shape: " + str(output_shape))
         self.output_dim = output_shape
         return output_shape
 
@@ -83,12 +74,10 @@ class DePool2D(UpSampling2D):
 def deconv2d_fast(x, kernel, strides=(1, 1), border_mode='valid', data_format='channels_last',
                   image_shape=None, filter_shape=None):
     '''
-    Run on cuDNN if available.
     border_mode: string, "same" or "valid".
     '''
     if data_format not in {'channels_first', 'channels_last'}:
         raise Exception('Unknown data_format ' + str(data_format))
-    #print("df: " + data_format)
     if data_format == 'channels_last':
         # TF uses the last dimension as channel dimension,
         # instead of the 2nd one.
@@ -96,11 +85,7 @@ def deconv2d_fast(x, kernel, strides=(1, 1), border_mode='valid', data_format='c
         # TF input shape: (samples, rows, cols, input_depth)
         # TH kernel shape: (depth, input_depth, rows, cols)
         # TF kernel shape: (rows, cols, input_depth, depth)
-        #x = x.dimshuffle((0, 3, 1, 2))
-        #print("X prior Shuffle " + str(x.shape))
         x = K.permute_dimensions(x, (0, 1, 2, 3))
-        #kernel = kernel.dimshuffle((2, 3, 0, 1))
-        #print("K prior Shuffle " + str(kernel.shape))
         kernel = K.permute_dimensions(kernel, (0, 1, 3, 2))
 
         if image_shape:
@@ -110,24 +95,6 @@ def deconv2d_fast(x, kernel, strides=(1, 1), border_mode='valid', data_format='c
             filter_shape = (filter_shape[3], filter_shape[2],
                             filter_shape[0], filter_shape[1])
 
-    # if _on_gpu() and dnn.dnn_available():
-    #     if border_mode == 'same':
-    #         assert (strides == (1, 1))
-    #         conv_out = dnn.dnn_conv(img=x,
-    #                                 kerns=kernel,
-    #                                 border_mode='full')
-    #         shift_x = (kernel.shape[2] - 1) // 2
-    #         shift_y = (kernel.shape[3] - 1) // 2
-    #         conv_out = conv_out[:, :,
-    #                    shift_x:x.shape[2] + shift_x,
-    #                    shift_y:x.shape[3] + shift_y]
-    #     else:
-    #         conv_out = dnn.dnn_conv(img=x,
-    #                                 conv_mode='cross',
-    #                                 kerns=kernel,
-    #                                 border_mode=border_mode,
-    #                                 subsample=strides)
-    # else:
         if border_mode == 'same':
             th_border_mode = 'full'
             assert (strides == (1, 1))
@@ -136,24 +103,10 @@ def deconv2d_fast(x, kernel, strides=(1, 1), border_mode='valid', data_format='c
         else:
             raise Exception('Border mode not supported: ' + str(border_mode))
 
-        #conv_out = T.nnet.conv2d(x, kernel, 
-        #   border_mode=th_border_mode,
-        #   subsample=strides,
-        #   filter_flip=False,  # <<<<< IMPORTANT 111, dont flip kern
-        #   input_shape=image_shape,
-        #   filter_shape=filter_shape)
-        print(x.shape)
-        print(kernel.shape)
         conv_out = K.conv2d(x, kernel, padding=border_mode, strides=strides, data_format=data_format)
-        if border_mode == 'same':
-            shift_x = (kernel.shape[2] - 1) // 2
-            shift_y = (kernel.shape[3] - 1) // 2
-            conv_out = conv_out[:, :,
-            shift_x:x.shape[2] + shift_x,
-            shift_y:x.shape[3] + shift_y]
     if data_format == 'channels_first':
-#        conv_out = conv_out.dimshuffle((0, 2, 3, 1))
-        conv_out = K.permute_dimensions(conv_out, (0, 2, 3, 1))
+        raise Exception('Theano backend not implemented.')
+        #conv_out = K.permute_dimensions(conv_out, (0, 2, 3, 1))
     return conv_out
 
 
@@ -210,28 +163,31 @@ class Deconvolution2D(Convolution2D):
     '''
     input_ndim = 4
 
-    def __init__(self, binded_conv_layer, nb_out_channels=1, *args, **kwargs):
+    def __init__(self, binded_conv_layer, filters, kernel_size, nb_out_channels=1, *args, **kwargs):
         self._binded_conv_layer = binded_conv_layer
         self.nb_out_channels = nb_out_channels
         #self.output_dim = self._binded_conv_layer.output_dim
         #print("input shape conv2d: I=" + str(self._binded_conv_layer.input_shape[2]))
-        kwargs['filters'] = self._binded_conv_layer.filters
-        kwargs['nb_row'] = self._binded_conv_layer.input_shape[1]
-        kwargs['nb_col'] = self._binded_conv_layer.input_shape[2]
-        super(Deconvolution2D, self).__init__(*args, **kwargs)
+#        kwargs['filters'] = self._binded_conv_layer.filters
+#        kwargs['nb_row'] = self._binded_conv_layer.input_shape[1]
+#        kwargs['nb_col'] = self._binded_conv_layer.input_shape[2]
+        self.filters = filters
+        self.kernel_size = kernel_size
+#        self.input_shape = self._binded_conv_layer.input_shape
+        super(Deconvolution2D, self).__init__(filters=filters, kernel_size=kernel_size, *args, **kwargs)
 
 
     def build(self, input_shape):
         print("data format: " + str(self._binded_conv_layer.data_format))
         if self.data_format == 'channels_first':
-            channel_axis = 1
+            #channel_axis = 1
+            raise Exception('Theano backend not implemented.')
         else:
             channel_axis = -1
         if input_shape[channel_axis] is None:
             raise ValueError('The channel dimension of the inputs '
                              'should be defined. Found `None`.')
         input_dim = input_shape[channel_axis]
-        print('input dim: ' + str(input_shape))
         self.kernel_shape = self.kernel_size + (input_dim, self.filters)
         self.bias = K.zeros((self.nb_out_channels,))
         self.params = [self.bias]
@@ -256,11 +212,11 @@ class Deconvolution2D(Convolution2D):
 #            del self.initial_weights
 
     def compute_output_shape(self, input_shape):
-        print("deconv2d output shape")
         output_shape = list(super(Deconvolution2D, self).compute_output_shape(input_shape))
 
         if self.data_format == 'channels_first':
-            output_shape[1] = self.nb_out_channels
+            #output_shape[1] = self.nb_out_channels
+            raise Exception('Theano backend not implemented.')
         elif self.data_format == 'channels_last':
             output_shape[3] = self.nb_out_channels
         else:
@@ -277,52 +233,47 @@ class Deconvolution2D(Convolution2D):
                                  image_shape=inputs.shape,
                                  filter_shape=self.kernel_shape)
         if self.data_format == 'channels_first':
-            output = conv_out + K.reshape(self.bias, (1, self.nb_out_channels, 1, 1))
+            raise Exception('Theano backend not implemented.')
+            #output = conv_out + K.reshape(self.bias, (1, self.nb_out_channels, 1, 1))
         elif self.data_format == 'channels_last':
             output = conv_out + K.reshape(self.bias, (1, 1, 1, self.nb_out_channels))
         else:
             raise Exception('Invalid dim_ordering: ' + self.dim_ordering)
-        #output = self.activation(output)
         return output
 
     def get_config(self):
         config = {'name': self.__class__.__name__,
-                  'nb_filter': self.nb_filter,
-                  'nb_row': self.nb_row,
-                  'nb_col': self.nb_col,
-                  'init': self.init.__name__,
+                  'filters': self.filters,
+                  'input_shape': self.input_shape,
+                  '_binded_conv_layer': self._binded_conv_layer.get_config(),
+                  'kernel_initializer': self.kernel_initializer.get_config() if self.kernel_initializer else None,
+                  'data_format': self.data_format,
                   'activation': self.activation.__name__,
-                  'border_mode': self.border_mode,
-                  'subsample': self.subsample,
-                  'dim_ordering': self.dim_ordering,
-                  'W_regularizer': self.W_regularizer.get_config() if self.W_regularizer else None,
-                  'b_regularizer': self.b_regularizer.get_config() if self.b_regularizer else None,
+                  'padding': self.padding,
+                  'strides': self.strides,
+                  'kernel_regularizer': self.kernel_regularizer.get_config() if self.kernel_regularizer else None,
+                  'bias_regularizer': self.bias_regularizer.get_config() if self.bias_regularizer else None,
                   'activity_regularizer': self.activity_regularizer.get_config() if self.activity_regularizer else None,
-                  'W_constraint': self.W_constraint.get_config() if self.W_constraint else None,
-                  'b_constraint': self.b_constraint.get_config() if self.b_constraint else None}
+                  'kernel_constraint': self.kernel_constraint.get_config() if self.kernel_constraint else None,
+                  'bias_constraint': self.bias_constraint.get_config() if self.bias_constraint else None}
         base_config = super(Convolution2D, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
 
 class DependentDense(Dense):
-    def __init__(self, output_dim, master_layer, init='glorot_uniform', activation='linear', weights=None,
-                 W_regularizer=None, b_regularizer=None, activity_regularizer=None,
-                 W_constraint=None, b_constraint=None, input_dim=None, **kwargs):
+    def __init__(self, units, master_layer, init='glorot_uniform', activation='linear', weights=None,
+                 kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None,
+                 kernel_constraint=None, bias_constraint=None, input_dim=None, **kwargs):
         self.master_layer = master_layer
-        self.units = output_dim
+        self.units = units
         self.initial_weights = weights
-        self.W_regularizer = W_regularizer
-        self.bias_regularizer = b_regularizer
+        self.kernel_regularizer = kernel_regularizer
+        self.bias_regularizer = bias_regularizer
         self.kernel = K.transpose(self.master_layer.kernel)
-#        print("units: " + str(output_dim))
-        super(DependentDense, self).__init__(output_dim, **kwargs)
+        super(DependentDense, self).__init__(units, **kwargs)
 
     def build(self, input_shape):
-        #self.bias = K.zeros((self.master_layer.units,))
-        #print(self.master_layer.kernel.shape)
-        input_dim = input_shape[-1]
-#        print("units@build: " + str(self.units))
-#        print("kernel units@build: " + str(self.kernel.shape))
+        #input_dim = input_shape[-1]
         self.bias = self.add_weight(shape=(self.units,),
                                         initializer=self.bias_initializer,
                                         name='bias',
@@ -346,3 +297,21 @@ class DependentDense(Dense):
         if self.initial_weights is not None:
             self.set_weights(self.initial_weights)
             del self.initial_weights
+            
+        def get_config(self):
+            config = {'name': self.__class__.__name__,
+                      'units': self.units,
+                      'input_shape': self.input_shape,
+                      'master_layer':  self.master_layer.get_config(),
+                      'kernel_initializer': self.kernel_initializer.get_config() if self.kernel_initializer else None,
+                      'data_format': self.data_format,
+                      'activation': self.activation.__name__,
+                      'padding': self.padding,
+                      'strides': self.strides,
+                      'kernel_regularizer': self.kernel_regularizer.get_config() if self.kernel_regularizer else None,
+                      'bias_regularizer': self.bias_regularizer.get_config() if self.bias_regularizer else None,
+                      'activity_regularizer': self.activity_regularizer.get_config() if self.activity_regularizer else None,
+                      'kernel_constraint': self.kernel_constraint.get_config() if self.kernel_constraint else None,
+                      'bias_constraint': self.bias_constraint.get_config() if self.bias_constraint else None}
+            base_config = super(Dense, self).get_config()
+            return dict(list(base_config.items()) + list(config.items()))
